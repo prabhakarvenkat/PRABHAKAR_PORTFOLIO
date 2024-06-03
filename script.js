755bb68b0556f06d675f31adf462a938ef979d56
PRABHAKAR V <prabhakar15v@gmail.com>
2024-06-03 10:17:18 +0530

    Files Upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.ham-icon');
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
